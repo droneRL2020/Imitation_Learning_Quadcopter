3daecb18d121f1c5b7ac4cb17d5a20ea8b5738e4
droneRL2020 <seoho0527@gmail.com>
2018-01-03 02:38:17 +0900

    training and testing script modified

--- roll_output.py
+++ roll_output.py
@@ -1,8 +1,10 @@
-from Imitation_Learning_Quad import Roll
+from imitation_learning import Roll
 from PIL import Image
 
 
 ###===== Gets test_image as input when drone flies
-#test_image = Image.open("D:\Imitation_Learning_Quadcopter\saved_image\image_%d.jpg" % 87)
-action = Roll()
-roll_output = action.denormalize_action(test_image)
+input_test_image = Image.open("D:\Imitation_Learning_Quadcopter\saved_image\image_%d.jpg" % 87)
+expert_1 = Roll(input_test_image)
+
+output_action = Roll.denormalize_action(expert_1)
+print(output_action)
